7a67ed85caa7e0ff2094bb1e124b64969d6cb9b6
Turtleboi1155 <maxx.hahnlen@gmail.com>
2026-01-14 15:48:01 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,14 @@
 import express from "express";
-import session from "express-session";
-import bcrypt from "bcrypt";
-import http from "http";
 import { createBareServer } from "@titaniumnetwork-dev/ultraviolet";
+import http from "http";
 
 const app = express();
 const server = http.createServer(app);
 const bare = createBareServer("/bare/");
 
-// CONFIG
-const PASSWORD_HASH = await bcrypt.hash("changeme123", 10);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(session({
-  secret: "supersecretkey",
-  resave: false,
-  saveUninitialized: false
-}));
-
-// Auth middleware
-function requireAuth(req, res, next) {
-  if (req.session.authed) return next();
-  res.redirect("/login");
-}
-
-// Static
 app.use(express.static("public"));
 
-// Login
-app.get("/login", (_, res) => {
-  res.sendFile(process.cwd() + "/public/login.html");
-});
-
-app.post("/login", async (req, res) => {
-  const ok = await bcrypt.compare(req.body.password, PASSWORD_HASH);
-  if (!ok) return res.send("Invalid password");
-
-  req.session.authed = true;
-  res.redirect("/");
-});
-
-// Protect proxy
-app.use("/uv/", requireAuth, (req, res) => {
+app.use("/uv/", (req, res) => {
   bare.routeRequest(req, res);
 });
 
@@ -54,5 +19,5 @@ server.on("upgrade", (req, socket, head) => {
 });
 
 server.listen(8080, () =>
-  console.log("Secure proxy on http://localhost:8080")
+  console.log("Proxy running on http://localhost:8080")
 );
